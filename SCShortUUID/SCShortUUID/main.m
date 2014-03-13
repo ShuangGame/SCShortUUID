488e0b441ac56932563666cd9621ff4d01b6b64d
Shuang CAO <18600561316@163.com>
2014-03-13 23:35:56 +0800

    project create

---
+++ SCShortUUID/SCShortUUID/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SCShortUUID
+//
+//  Created by Shuang on 14-3-13.
+//  Copyright (c) 2014年 Shuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
